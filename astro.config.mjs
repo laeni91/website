03944f186bc6e6579b4beb3457d0cb38287d21e9
huitquatredev <julien@huitquatre.fr>
2026-09-18 15:02:54 +0200

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import StoryblokClient from "storyblok-js-client";
 import devtoolsJson from "vite-plugin-devtools-json";
 import vercel from "@astrojs/vercel";
 
+import robotsTxt from "astro-robots-txt";
+
 // const rootDir = new URL(".", import.meta.url).pathname;
 // const modulePath = resolve(rootDir, "src", "generated", "sriHashes.mjs");
 
@@ -39,41 +41,38 @@ export default defineConfig({
   trailingSlash: "ignore",
   output: env.STORYBLOK_IS_PREVIEW === "yes" ? "server" : "static",
 
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      bridge: env.STORYBLOK_IS_PREVIEW === "yes",
-      components: {
-        page: "storyblok/Page",
-        blog: "storyblok/Blog",
-        theme: "storyblok/global/Theme",
-        hero: "storyblok/Hero",
-        featureSimple: "storyblok/FeatureSimple",
-        featureGalleryIcons: "storyblok/FeatureGalleryIcons",
-        featureBgImageCard: "storyblok/FeatureBgImageCard",
-        faq: "storyblok/Faq",
-        timeline: "storyblok/Timeline",
-        testimonialStats: "storyblok/TestimonialStats",
-        logoGallery: "storyblok/LogoGallery",
-        contactForm: "storyblok/ContactForm",
-        contactInfo: "storyblok/ContactInfo",
-        blocText: "storyblok/BlocText",
-        stepsRows: "storyblok/StepsRows",
-        stepsCols: "storyblok/StepsCols",
-        testimonialSimple: "storyblok/TestimonialSimple",
-        testimonialsMultiple: "storyblok/TestimonialsMultiple",
-        team: "storyblok/Team",
-        horizontalGallery: "storyblok/GalleryHorizontal",
-        masonryGallery: "storyblok/GalleryMasonry",
-        image: "storyblok/ImageFullHeight",
-        blogGallery: "storyblok/BlogGallery",
-        slider: "storyblok/Slider"
-      },
-    }),
-    sitemap({
-      filter: (page) => !filteredUrls.includes(page),
-    }),
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    bridge: env.STORYBLOK_IS_PREVIEW === "yes",
+    components: {
+      page: "storyblok/Page",
+      blog: "storyblok/Blog",
+      theme: "storyblok/global/Theme",
+      hero: "storyblok/Hero",
+      featureSimple: "storyblok/FeatureSimple",
+      featureGalleryIcons: "storyblok/FeatureGalleryIcons",
+      featureBgImageCard: "storyblok/FeatureBgImageCard",
+      faq: "storyblok/Faq",
+      timeline: "storyblok/Timeline",
+      testimonialStats: "storyblok/TestimonialStats",
+      logoGallery: "storyblok/LogoGallery",
+      contactForm: "storyblok/ContactForm",
+      contactInfo: "storyblok/ContactInfo",
+      blocText: "storyblok/BlocText",
+      stepsRows: "storyblok/StepsRows",
+      stepsCols: "storyblok/StepsCols",
+      testimonialSimple: "storyblok/TestimonialSimple",
+      testimonialsMultiple: "storyblok/TestimonialsMultiple",
+      team: "storyblok/Team",
+      horizontalGallery: "storyblok/GalleryHorizontal",
+      masonryGallery: "storyblok/GalleryMasonry",
+      image: "storyblok/ImageFullHeight",
+      blogGallery: "storyblok/BlogGallery",
+      slider: "storyblok/Slider"
+    },
+  }), sitemap({
+    filter: (page) => !filteredUrls.includes(page),
+  }), robotsTxt()],
 
   site: siteurl,
 
